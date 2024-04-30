88a6d9bbce178f643c2c75c5c2784553ec40cbec
Alice Ke <aliceke64@gmail.com>
2024-04-30 13:11:37 -0400

    adding routes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import json
 
 from datetime import datetime
+
 # Added model/classes here
 from db import db, User, Food
 from flask import Flask, request
@@ -17,27 +18,30 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-#responses 
+
+# responses
 # generalized response formats
 def success_response(data, code=200):
     return json.dumps(data), code
 
+
 def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
+
 # your routes here
 @app.route("/")
-
 # Get All Food
 @app.route("/api/foods/", methods=["GET"])
 def get_all_foods():
     """
     Endpoint for getting all food logged
     """
-    
-    return success_response({"foods":[f.food_serialize() for f in Food.query.all()]})
 
-# Create New Food 
+    return success_response({"foods": [f.food_serialize() for f in Food.query.all()]})
+
+
+# Create New Food
 # Not working? ? change to user_id for rest?  "/api/user/<int:user_id>/foods/"
 @app.route("/api/foods/", methods=["POST"])
 def create_food():
@@ -45,7 +49,7 @@ def create_food():
     Endpoint for Creating Food
     """
     # add user_id to the params? search by user id
-    #however there are only 1 user 
+    # however there are only 1 user
 
     # user = User.query.filter_by(id=user_id). first()
     # if user is None:
@@ -54,22 +58,22 @@ def create_food():
     body = json.loads(request.data)
 
     new_food = Food(
-        name = body.get("name"),
-        calories = body.get("calories"),
-        carbs = body.get("carbs"),
-        protein = body.get("protein"),
-        fat = body.get("fat"),
+        name=body.get("name"),
+        calories=body.get("calories"),
+        carbs=body.get("carbs"),
+        protein=body.get("protein"),
+        fat=body.get("fat"),
         # add timestamp?
-        timestamp = datetime.utcnow()
-
+        timestamp=datetime.utcnow(),
     )
 
     db.session.add(new_food)
     db.session.commit()
 
     return success_response(new_food.food_serialize(), 201)
-    
-# Get food by id 
+
+
+# Get food by id
 @app.route("/api/foods/<int:food_id>/", methods=["GET"])
 def get_course(food_id):
     """
@@ -96,6 +100,7 @@ def delete_course(food_id):
     db.session.commit()
     return success_response(food.food_serialize())
 
+
 # Create User
 @app.route("/api/users/", methods=["POST"])
 def create_user():
@@ -105,14 +110,16 @@ def create_user():
     body = json.loads(request.data)
 
     new_user = User(
-        name = body.get("name"),
-        weight = body.get("weight"),
-        height = body.get("height"),
-        goal_weight = body.get("goal_weight")
+        name=body.get("name"),
+        weight=body.get("weight"),
+        height=body.get("height"),
+        goal_weight=body.get("goal_weight"),
     )
     db.session.add(new_user)
     db.session.commit()
     return success_response(new_user.user_serialize(), 201)
+
+
 # Get a user by ID
 @app.route("/api/users/<int:user_id>/", methods=["GET"])
 def get_user_by_id(user_id):
@@ -122,6 +129,26 @@ def get_user_by_id(user_id):
     user = User.query.filter_by(id=user_id).first()
     if user is None:
         return failure_response("User not found")
+    return success_response(user.user_serialize())
+
+
+# Update user's weight
+@app.route("/api/users/<int:user_id>/", methods=["POST"])
+def update_user_info(user_id):
+    """
+    Endpoint - Update a user's weight
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    body = json.loads(request.data)
+    if None in (body, body.get("weight"), body.get("type")):
+        return failure_response("Missing required fields.", 400)
+    if body.get("type") == "weight":
+        user.weight = body.get("weight")
+    elif body.get("type") == "height":
+        user.height = body.get("height")
+    db.session.commit()
     return success_response(user.user_serialize())
 
 
