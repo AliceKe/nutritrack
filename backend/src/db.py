68a6eff5b31177f15972157a24869c84917c1bb4
Alice Ke <aliceke64@gmail.com>
2024-05-03 15:30:02 -0400

    adding user table

--- backend/src/db.py
+++ backend/src/db.py
@@ -5,7 +5,12 @@ from datetime import datetime
 
 db = SQLAlchemy()
 # association tables
-# TBD
+user_food_association = db.Table(
+    "user_food_association",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("food_id", db.Integer, db.ForeignKey("food.id"), primary_key=True),
+)
+
 # your classes here
 
 
@@ -23,7 +28,8 @@ class User(db.Model):
     daily_calorie_target = db.Column(db.Integer, nullable=False)
 
     # cascades with user, if user deleted, delete the foods under user
-    foods = db.relationship("Food", cascade="delete")
+    # foods = db.relationship("Food", cascade="delete")
+    foods = db.relationship("Food", secondary=user_food_association, backref="users")
 
     def __init__(self, **kwargs):
         """
@@ -68,6 +74,7 @@ class Food(db.Model):
     protein = db.Column(db.Float, nullable=False)
 
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    users = db.relationship("User", secondary=user_food_association, backref="foods")
 
     # should 1-many connect to user
     # reference id of user table
@@ -99,5 +106,4 @@ class Food(db.Model):
             "fat": self.fat,
             "protein": self.protein,
             "timestamp": self.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-            "user_id": self.user_id,
         }
